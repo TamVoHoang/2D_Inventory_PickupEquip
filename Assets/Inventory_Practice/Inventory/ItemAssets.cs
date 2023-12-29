3390459cd4d15e9cf753c8bae70737e4ef81a314
TamVoHoang <130343021+TamVoHoang@users.noreply.github.com>
2023-12-29 18:56:39 +0700

    Dec-29
    first commit

---
+++ Assets/Inventory_Practice/Inventory/ItemAssets.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAssets : MonoBehaviour
+{
+    //? gamobject = doi tuong  ItemAssets chua toan bo hinh anh de hien thi cua Item
+    public static ItemAssets Instance {get; private set;}
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    public Transform pfItemWorld;
+
+    public Sprite swordSprite;
+    public Sprite healthPotionSprite;
+    public Sprite manaPotionSprite;
+    public Sprite coinSprite;
+    public Sprite medkit;
+}
